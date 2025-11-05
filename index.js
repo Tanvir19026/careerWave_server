5ff7536ac68b338bfa00aee3f40d1347ea25c75e
Tanvir19026 <tanvirrafi1999@gmail.com>
2025-11-05 20:59:02 +0600

    finally deploy

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"], // React frontend URL
+    origin: ["https://magenta-arithmetic-a45d97.netlify.app/"], // React frontend URL
     credentials: true,
   })
 );
